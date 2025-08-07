57c91b580d2947533c85ce90b127713da9ddc090
bomkim <bomkim@iwi.co.kr>
2025-08-07 15:54:21 +0900

    .

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ const Home = lazy(() => import('./pages/Home'));
 const Today = lazy(() => import('./pages/Today'));
 const Webd = lazy(() => import('./pages/Webd'));
 const Port = lazy(() => import('./pages/Port'));
-// const Not = lazy(() => import('./pages/Not'));
 
 const App = () => {
     return (
@@ -19,7 +18,6 @@ const App = () => {
                     <Route path="/webd" element={<Webd />} />
                     <Route path="/port" element={<Port />} />
                     <Route path="/port/:id" element={<PortDetail />} />
-                    {/* <Route path="*" element={<Not />} /> */}
                 </Routes>
             </Suspense>
         </BrowserRouter>
